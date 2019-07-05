4245a556b432a39a69db1be0469c071e6ed40675
Emmanuel Ramirez <emmanuel.ramirezg@outlook.com>
2019-07-05 12:31:46 -0700

    Quitando BUG, verificación al crear usuario

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
   var desde = req.query.desde || 0;
   desde = Number(desde);
 
-  Usuario.find({}, 'nombre email img role')
+  Usuario.find({}, 'nombre email img role google')
     .skip(desde)
     .limit(5)
     .exec((err, usuarios) => {
@@ -33,7 +33,7 @@ router.get('/', (req, res) => {
         res.status(200).json({
           ok: true,
           usuarios: usuarios,
-          page: desde + 1 + ' de ' + (desde + 5),
+          page: desde + 1 + ' al ' + (desde + 5),
           total: count
         });
       });
@@ -62,14 +62,14 @@ router.get('/', (req, res) => {
 // ===================================================================================================
 // POST -- Crear un nuevo usuario
 // ===================================================================================================
-router.post('/', verificarToken, (req, res) => {
+router.post('/', (req, res) => {
   var body = req.body;
   var usuario = new Usuario({
     nombre: body.nombre,
     email: body.email,
     password: bcrypt.hashSync(body.password, 10),
     img: body.img,
-    role: body.role.toUpperCase()
+    role: body.role
   });
 
   usuario.save((err, usuarioGuardado) => {
